5c0a7b59ae63a479860f4bfce907a7e7d30d7c64
Matthew Brantley <Raltrwx@gmail.com>
2016-07-05 17:57:19 -0600

    Brackets again

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -3,21 +3,21 @@ module.exports = function () {
     function(energy, roleName) {
         var numberOfParts = Math.floor(energy / 350);
         var body = [];
-        for (let i = 0; i < numberOfParts; i++) {
-            body.push(WORK);
-        }
-                for (let i = 0; i < numberOfParts; i++) {
-                    body.push(WORK);
-                }
-                for (let i = 0; i < numberOfParts; i++) {
-                    body.push(CARRY);
-                }
-                for (let i = 0; i < numberOfParts; i++) {
-                    body.push(MOVE);
-                }
-                for (let i = 0; i < numberOfParts; i++) {
-                    body.push(MOVE);
-                }
+            for (let i = 0; i < numberOfParts; i++) {
+                body.push(WORK);
+            }
+            for (let i = 0; i < numberOfParts; i++) {
+                body.push(WORK);
+            }
+            for (let i = 0; i < numberOfParts; i++) {
+                body.push(CARRY);
+            }
+            for (let i = 0; i < numberOfParts; i++) {
+                body.push(MOVE);
+            }
+            for (let i = 0; i < numberOfParts; i++) {
+                body.push(MOVE);
+            }
         return this.createCreep(body, undefined, {role: roleName, working: false });
     };
 
