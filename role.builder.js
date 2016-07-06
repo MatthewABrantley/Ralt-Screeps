631292a0a1d9c0068fa79e992b6502136a5d6fae
Matthew Brantley <Raltrwx@gmail.com>
2016-07-05 20:32:53 -0600

    Trying Reno's fix for builders basically being upgraders

--- role.builder.js
+++ role.builder.js
@@ -12,7 +12,7 @@ module.exports = {
 
         if (creep.memory.working == true) {
             var constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-            if (constructionSite != undefined) {
+           if (constructionSite != STRUCTURE_CONTROLLER) {
                 if (creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(constructionSite);
                 }
