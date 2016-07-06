898091405d85b84eb9d19f600f88b333497c1e67
Matthew Brantley <Raltrwx@gmail.com>
2016-07-06 16:27:36 -0600

    Fixed = to be == in wallrepairer filter

--- role.wallrepairer.js
+++ role.wallrepairer.js
@@ -12,9 +12,8 @@ module.exports = {
 
         if (creep.memory.working == true) {
             var structure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (s) => s.hits < s.hitsMax && s.structureType = STRUCTURE_WALL
+                filter: (s) => s.hits < s.hitsMax && s.structureType == STRUCTURE_WALL
             });  
-            
             if (structure != undefined) {
                 if (creep.repair(structure) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(structure);
