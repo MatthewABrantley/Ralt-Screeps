b047dd393cf7d6a218f53908a75d5b7ce93e3916
Matthew Brantley <Raltrwx@gmail.com>
2016-07-06 19:33:12 -0600

    Yeah that fixed it, but now it will occupy my harvesters and kill my creep population

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ module.exports.loop = function () {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => structure.hits < structure.hitsMax
         });
-        if(closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
+        //if(closestDamagedStructure) {
+        //    tower.repair(closestDamagedStructure);
         }
         var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if(closestHostile) {
