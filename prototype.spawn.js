626536d372be8d1cbd99a5cadb74221da84433bb
Matthew Brantley <Raltrwx@gmail.com>
2016-07-07 20:43:44 -0600

    Added a console.log to see if I can debug this stupid prototype.spawn

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -1,12 +1,15 @@
 module.exports = function () {
     StructureSpawn.prototype.createCustomCreep = 
     function(energy, roleName) {
+        // Defined variable numberOfParts
         var numberOfParts = Math.floor(energy / 750);
+        // Define Body to be an array
         var body = [];
+            // 
             for (let i = 0; i < numberOfParts; i++) {
                 body.push(WORK);
             }
-                        for (let i = 0; i < numberOfParts; i++) {
+            for (let i = 0; i < numberOfParts; i++) {
                 body.push(WORK);
             }
             for (let i = 0; i < numberOfParts; i++) {
@@ -26,6 +29,9 @@ module.exports = function () {
             }
             for (let i = 0; i < numberOfParts; i++) {
                 body.push(MOVE);
+                    if (i != undefined) {
+                        console.log("numberOfParts" + i);
+                }
             }
         return this.createCreep(body, undefined, {role: roleName, working: false });
     };
