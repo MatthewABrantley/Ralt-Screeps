44fcbb2f781e9541787061bd84041ae03f3ccfdf
Raltrwx <Raltrwx@gmail.com>
2016-07-14 18:19:08 -0600

    Filename Fix

---
+++ role.harvesterclose.js
@@ -0,0 +1,35 @@
+module.exports = {
+    run: function(creep) {
+            if (creep.memory.working == true && creep.carry.energy == 0) {
+            creep.memory.working = false;
+        }
+        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.working = true;
+        }
+
+        if (creep.memory.working == true) {
+            var structure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+              filter: (s) => (s.structureType == STRUCTURE_STORAGE
+                            || s.structureType == STRUCTURE_CONTAINER
+                            || s.structureType == STRUCTURE_EXTENSION)
+                            && s.energy < s.energyCapacity 
+            });
+            if (structure != undefined) {
+                if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(structure);
+                }
+            }   
+        }
+        else {
+            var source = Game.getObjectById('576a9bd757110ab231d880ca');
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
+            }    
+        }
+    }
+};
+
+
+
+// var source = 
+// creep.memory.sourceId = source.id;
