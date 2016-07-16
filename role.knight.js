8956e02aee5fe6f02730289db8bc01247ab72630
Raltrwx <Raltrwx@gmail.com>
2016-07-16 05:16:43 -0600

    Knight fix?

--- role.knight.js
+++ role.knight.js
@@ -20,9 +20,9 @@ module.exports = {
         }
         // This is what happens if false, false is not directly referenced, it has a built in limit to two states unless I write another else if above for it
         else {
-            var targets = Game.flags.Flag1.findEnemiesInRange(5);
-            if (targets.length > 0) {
-                creep.moveTo(Game.flags.Flag1);
+            var targets = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
+            if (targets.length > 0 && Game.cpu.tickLimit - Game.cpu.getUsed() > 40) {
+                creep.moveTo(new RoomPosition(5, 38, 'W32S37'));
             //if (creep.withdraw(pickupStorage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
             //    creep.moveTo(pickupStorage);
             }    
