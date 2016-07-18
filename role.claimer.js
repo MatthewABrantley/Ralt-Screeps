da6cae0e79be505c7311ff1d3735160dd282f773
Raltrwx <Raltrwx@gmail.com>
2016-07-17 21:52:56 -0600

    Working on claimer

--- role.claimer.js
+++ role.claimer.js
@@ -1,45 +1,37 @@
 //Module to Export
 module.exports = {
     run: function(creep) {
-// Define working to == IS IN ENEMY ROOM, maybe implement a WAITING stage to wait for a healer to link up with him
-        if (creep.memory.working == false && creep.room == W23S38) {
-            var exitDir = FIND_EXIT_TOP;
-            var exit = creep.pos.findClosestByRange(exitDir);
+        if (creep.memory.working == false && creep.room != 'W33S39') {
+            var route = Game.map.findRoute(creep.room, anotherRoomName);
+            if(route.length > 0) {
+            console.log('Now heading to room '+route[0].room);
+            var exit = creep.pos.findClosestByRange(route[0].exit);
             creep.moveTo(exit);
             }
-            else
-                if (creep.memory.working == false && creep.room == W32S37) {
-                    var exitDir = FIND_EXIT_LEFT;
-                    var exit = creep.pos.findClosestByRange(exitDir);
-                    creep.moveTo(exit);
-                    }
-                    else
-                        if (creep.memory.working == false && creep.room == W23S37) {
-                            creep.memory.working == true;
-    }
+        }
+        
+            
+
+
+
                     //else 
                      //   if (creep.memory.working == false) {
                       //      creep.memory.working = true;
                     //        }
 // if creep.memory.working == true find enemy units and attack
         if (creep.memory.working == true) {
-            var kill = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
-            if (kill != undefined) {
-                if (creep.attack(kill) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(kill);
-                }
-            }   
-        }
-// This is what happens if false, false is not directly referenced, it has a built in limit to two states unless I write another else if above for it
-        else 
-            var takeControl = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES);
+            var takeControl = creep.pos.findClosestByPath(FIND_STRUCTURES);
             if (takeControl != undefined) {
                 if(creep.claimController(takeControl) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(takeControl);
                 }
             }
+        }
     }
 }
+// This is what happens if false, false is not directly referenced, it has a built in limit to two states unless I write another else if above for it
+ 
+
 //
 //if(creep.room != anotherRoomName) {
 //    var exitDir = Game.map.findExit(creep.room, anotherRoomName);
